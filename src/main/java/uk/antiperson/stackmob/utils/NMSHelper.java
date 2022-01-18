11a7d136732ab68ee697cc1f96209b315a54889e
Aurélien <aurelien.domino@gmail.com>
2022-01-18 09:59:19 +0100

    Bump version, revert temporary baby zombie workaround (fixed by Paper).

--- src/main/java/uk/antiperson/stackmob/utils/NMSHelper.java
+++ src/main/java/uk/antiperson/stackmob/utils/NMSHelper.java
@@ -4,28 +4,12 @@ import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
 import net.minecraft.network.syncher.DataWatcher;
 import net.minecraft.network.syncher.DataWatcherObject;
 import net.minecraft.network.syncher.DataWatcherRegistry;
-import net.minecraft.world.entity.EntityInsentient;
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftEntity;
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_18_R1.entity.CraftZombie;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Zombie;
-
-import java.lang.reflect.Field;
 
 public class NMSHelper {
-
-    private static Field XP_REWARD;
-
-    static {
-        try {
-            XP_REWARD = EntityInsentient.class.getDeclaredField("bM");
-            XP_REWARD.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void sendPacket(Player player, Entity entity, boolean tagVisible) {
         final CraftEntity craftEntity = (CraftEntity) entity;
@@ -33,14 +17,6 @@ public class NMSHelper {
         watcher.a(new DataWatcherObject<>(3, DataWatcherRegistry.i), tagVisible);
         PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadata(craftEntity.getEntityId(), watcher, true);
         ((CraftPlayer) player).getHandle().b.a(packet);
-    }
-
-    public static void resetBabyZombieExp(Zombie zombie) {
-        try {
-            XP_REWARD.set(((CraftZombie) zombie).getHandle(), 5);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
     }
 
 }
