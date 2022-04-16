4019ffcc24fe756ddbe75794846b50240bfe9a0c
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2022-04-17 01:03:25 +0200

    Update DropListener.java

--- src/main/java/uk/antiperson/stackmob/listeners/DropListener.java
+++ src/main/java/uk/antiperson/stackmob/listeners/DropListener.java
@@ -1,8 +1,8 @@
 package uk.antiperson.stackmob.listeners;
 
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -24,7 +24,7 @@ public class DropListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onDropListener(EntityDropItemEvent event) {
-        if (event.getEntity() != EntityType.VILLAGER &&
+        if (!(event.getEntity() instanceof Villager) &&
                 event.getItemDrop().getItemStack().getType() != Material.EGG &&
                 event.getItemDrop().getItemStack().getType() != Material.SCUTE) {
             return;
