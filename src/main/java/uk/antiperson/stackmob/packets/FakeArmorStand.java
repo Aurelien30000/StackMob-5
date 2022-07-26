e47fc681e4c01f1d38039c2939f1c56dd58bae21
Aurelien30000 <aurelien.domino@gmail.com>
2022-07-26 11:38:19 +0200

    Update

--- src/main/java/uk/antiperson/stackmob/packets/FakeArmorStand.java
+++ src/main/java/uk/antiperson/stackmob/packets/FakeArmorStand.java
@@ -15,9 +15,8 @@ public interface FakeArmorStand {
     void removeFakeArmorStand();
 
     default Location adjustLocation(Entity entity) {
-        double adjustment = entity.getCustomName() == null || entity.getCustomName().length() == 0 ? 0.1 : 0.3;
+        double adjustment = entity.customName() == null || entity.customName() == Component.empty() ? 0.1 : 0.3;
         return entity.getLocation().add(0, entity.getHeight() + adjustment, 0);
     }
-
 
 }
