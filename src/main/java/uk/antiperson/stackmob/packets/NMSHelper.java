2f4998d05eb1e10360e80168b07ba40f4caf0b68
Aurélien <aurelien.domino@gmail.com>
2023-06-08 21:07:46 +0200

    1.20 update

--- src/main/java/uk/antiperson/stackmob/packets/NMSHelper.java
+++ src/main/java/uk/antiperson/stackmob/packets/NMSHelper.java
@@ -6,8 +6,8 @@ import net.minecraft.network.syncher.DataWatcherObject;
 import net.minecraft.network.syncher.DataWatcherRegistry;
 import net.minecraft.world.entity.EntityLiving;
 import net.minecraft.world.entity.boss.wither.EntityWither;
-import org.bukkit.craftbukkit.v1_19_R3.entity.CraftLivingEntity;
-import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R1.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -19,11 +19,11 @@ public class NMSHelper {
     public static void sendVisibilityPacket(Player player, Entity entity, boolean tagVisible) {
         DataWatcher.b<Boolean> af = DataWatcher.b.a(new DataWatcherObject<>(3, DataWatcherRegistry.k), tagVisible);
         PacketPlayOutEntityMetadata packetPlayOutEntityMetadata = new PacketPlayOutEntityMetadata(entity.getEntityId(), Collections.singletonList(af));
-        ((CraftPlayer) player).getHandle().b.a(packetPlayOutEntityMetadata);
+        ((CraftPlayer) player).getHandle().c.a(packetPlayOutEntityMetadata);
     }
 
     public static boolean canCreateWitherRose(LivingEntity entity) {
         EntityLiving entityLiving = ((CraftLivingEntity) entity).getHandle();
-        return entityLiving.eD() instanceof EntityWither;
+        return entityLiving.eH() instanceof EntityWither;
     }
 }
